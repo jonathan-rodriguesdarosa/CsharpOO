146bb88a557794ab416161755fff3c111d62804e
jonathan-rodriguesdarosa <126508209+jonathan-rodriguesdarosa@users.noreply.github.com>
2023-03-28 16:13:55 -0300

    Aula 03 variáveis

---
+++ Aula03/aula03.cs
@@ -0,0 +1,14 @@
+using System;
+class aula03 {
+    static void Main() {
+        byte n1=10; // 0 e 255
+        int num=0;
+        char letra= 'c';
+        float valor=5.3f;
+        string nome="Jonathan";
+
+        var aux=nome;
+
+        Console.WriteLine(aux);
+    }
+}
